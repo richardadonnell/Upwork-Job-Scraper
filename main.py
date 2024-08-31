267fe7afc234a6e3b53782c338d0c3d589a0afca
warezit <warezit@gmail.com>
2024-08-31 18:18:26 -0400

    added ability to send data to make.com webhook

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from selenium import webdriver
 from job_feed import find_jobs
 from selenium_stealth import stealth
+import json
+import os
+import requests
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# Get webhook URL from .env file
+webhook_url = os.getenv('WEBHOOK_URL')
 
 options = webdriver.ChromeOptions()
-#options.add_experimental_option("detach", True)
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
 options.add_argument("--headless=new")
@@ -21,6 +30,20 @@ stealth(browser,
         fix_hairline=True,
         )
 
-
+# Find jobs and save to JSON file
 find_jobs(browser=browser)
 
+# Read JSON file
+with open('jobs_data.json', 'r') as file:
+    data = json.load(file)
+
+# Send data to webhook as a single array
+response = requests.post(webhook_url, json={"jobs": data})
+
+if response.status_code == 200:
+    print("Data successfully sent to webhook")
+else:
+    print(f"Failed to send data. Status code: {response.status_code}")
+
+# Close the browser
+browser.quit()
