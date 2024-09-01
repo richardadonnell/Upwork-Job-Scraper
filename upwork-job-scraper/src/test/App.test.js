aacc049ad2258f10e76726dea1cb9de79049c60c
warezit <warezit@gmail.com>
2024-09-01 15:36:46 -0400

    cleaning up files

--- upwork-job-scraper/src/test/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Popup from '../main/popup/Popup';
-
-test('renders learn react link', () => {
-  render(<Popup />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
